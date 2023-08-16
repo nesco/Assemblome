924677fcc799a780965eae8e13e04122cd00dc80
Emmanuel Federbusch <emmanuel@MacBook-Pro-de-Emmanuel-2.local>
2023-08-16 18:26:14 +0200

    Adding the first example of a assemblome file

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 def scan(path):
     """read a asb file and perform a lexical analysis."""
     
-    instructions = ['produce', 'raw']
+    instructions = ['tag', 'produce', 'raw']
+    content = []
     
     with open(path, 'r') as file:
-        content = list(file)
+        content = list(map(str.strip, file))
     
     return content
+
+
+def parse_tag(content):
+    pass
 
