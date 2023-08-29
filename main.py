aec70e8d7a77a0803bb51fcf606e962dc793e1ab
Emmanuel Federbusch <emmanuel@MacBook-Pro-de-Emmanuel-2.local>
2023-08-29 14:44:07 +0200

    tag instruction working as intended. Using reserved keyword as tag sgould be banned later

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 
 def scan(path):
     """read a asb file and perform a lexical analysis."""
@@ -11,6 +12,32 @@ def scan(path):
     return content
 
 
-def parse_tag(content):
-    pass
 
+def find_tag_pattern(text):
+    data, tag = None, None
+
+    # Define the regular expression pattern
+    pattern = r'^tag "(?P<data>[^"]+)" as (?P<tag>\w+)$'
+
+    # Match the pattern in the text
+    match = re.fullmatch(pattern, text)
+
+    if match:
+        data = match.group('data')
+        tag = match.group('tag')
+        
+    return data, tag
+
+def parse_tag(content):
+
+    content_new = []
+
+    for i, line in sorted(enumerate(content), key=lambda x: x[0], reverse=True):
+        data, tag = find_tag_pattern(line)
+        if data is not None and tag is not None:
+            content_new = [line.replace(tag, data) for line in content_new]
+        else:
+            content_new.append(line)
+
+    content_new.reverse()
+    return content_new
