bdfa00419b294d7d183ae19296830058de4f942e
Emmanuel Federbusch <emmanuel@MacBook-Pro-de-Emmanuel-2.local>
2023-09-07 17:09:58 +0200

    Added the base64 utils to encode the complement list. It will help reduce the lenght of the complement chain to produce a valid RNA chain from a amino-acid chain or a protein bank ID

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 import re
 
+### Info utils
+
+def baseX_to_baseY(num_str, base_x, base_y):
+    """Convert a number from base-x to base-y using string manipulation."""
+    chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/"
+    char_to_int = {char: i for i, char in enumerate(chars)}
+
+    num_base10 = 0
+    for digit in num_str:
+        num_base10 = num_base10 * base_x + char_to_int[digit]
+
+    if num_base10 == 0:
+        return "0"
+
+    num_baseY = ""
+    while num_base10 > 0:
+        num_base10, remainder = divmod(num_base10, base_y)
+        num_baseY = chars[remainder] + num_baseY
+
+    return num_baseY
+
+def list_to_base64(lst):
+    """Convert a list of integers [0, 5] to a base-64 string."""
+    # Add a sentinel value '6' to preserve leading zeros
+    lst_with_sentinel = [6] + lst
+    num_base7 = ''.join(map(str, lst_with_sentinel))
+    return baseX_to_baseY(num_base7, 7, 64)
+
+def base64_to_list(base64_str):
+    """Convert a base-64 string to a list of integers [0, 5]."""
+    num_base7 = baseX_to_baseY(base64_str, 64, 7)
+    # Remove the sentinel value '6' to get the original list
+    return [int(digit) for digit in num_base7[1:]]
+
 #### Genomics utils
 
 def specify(aa_chain, complement):
@@ -38,7 +72,7 @@ def specify(aa_chain, complement):
       aa_chain += ' '
 
     # Check if the two input chains have the same length
-    if len(aa_chain) != len(num_chain):
+    if len(aa_chain) != len(complement):
         raise ValueError("The amino acid chain and the number chain must have the same length.")
 
     # Initialize the RNA sequence with the start codon
@@ -46,7 +80,7 @@ def specify(aa_chain, complement):
 
     # Go through each amino acid and pick the corresponding codon
     for i, aa in enumerate(aa_chain):
-        num = num_chain[i]
+        num = complement[i]
 
          # Check if the number is valid for the given amino acid
         if num >= len(codon_table[aa]):
