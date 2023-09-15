fa854da68066ca5c1c07c71b3ddaa3875f907167
Emmanuel Federbusch <emmanuel@MacBook-Pro-de-Emmanuel-2.local>
2023-09-16 00:09:46 +0200

    At last, got the merge is finished: Clément's architecure has been adopted, with the latest code instead. Now main.py is as minimal as possible and most of the code has been transfered to lang.py, utils.py and utils_genomics.py. The Readme is still to be updated

--- main.py
+++ main.py
@@ -1,45 +1,8 @@
 import re
 
 from lang import parse_tags, parse_imports, parse_functional_expressions
-# from tag import Tag
-# from relationship import Relationship
 
 
-#############
-
-
-# def parse_tag(parts):
-#     """Parse a line meant to define a tag"""
-# 
-#     try:
-#         tag_value = parts[1].strip('"')
-#         tag_alias = parts[3]
-#         return Tag(tag_alias, tag_value)
-#     except Exception as e: #Parsing error
-#         print('Error: Cannot parse tag in line: {}'.format(" ".join(parts)))
-#         print('- ', e)
-#         return None
-# 
-# 
-# def parse_relationship(parts, tags):
-#     """Parse a line meant to define a relationship"""
-# 
-#     try:
-#         source_name, target_name = parts[1].split('@')
-#         if source_name in tags:
-#             source_name = tags[source_name].getValue()
-# 
-#         if target_name in tags:
-#             target_name = tags[target_name].getValue()
-# 
-#         return Relationship(source_name, target_name)
-#     except Exception as e:
-#         #Parsing error?
-#         print(e)
-#         print('Error: Cannot parse relationship in line: {}'.format(" ".join(parts)))
-#         return None
-
-#### Lang 
 def scan(path):
     """read a asb file and perform a lexical analysis."""
     
@@ -54,36 +17,11 @@ def scan(path):
         content = list(map(str.strip, file))
 
     content = [line for line in content if not line.startswith("#") and not line == ""]
-#    print(content)
-#    
-#    #Line by line analysis
-#    for line in content:
-#        parts = line.split()
-#
-#        if parts[0] == 'tag' and parts[2] == 'as':
-#            tag = parse_tag(parts)
-#            if tag:
-#                tags[tag.getAlias()] = tag
-#
-#        elif parts[0] == 'produce':
-#            relationship = parse_relationship(parts, tags)
-#            if relationship and relationship.to_str() not in relationships:
-#                relationships[relationship.to_str()] = relationship
-#
-#    return content, tags, relationships
     return content
-
-
-# =====
-# if __name__ == '__main__':
-#    content, tags, relationships = scan('instruct.asb')
-#
-#    # Print the parsed tags and relationships
-#    for tag in tags.values():
-#        print(tag)
-#    for rel in relationships.values():
-#        print(rel)
-#=======
 
 def parse(content):
     return parse_functional_expressions(parse_tags(parse_imports(content)))
+
+if __name__ == '__main__':
+    content = scan('instruct.asb')
+    output = parse(content)
